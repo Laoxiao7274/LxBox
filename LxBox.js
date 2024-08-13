6e8568b19a03d55f966283caae45b71d14db0d55
Laoxiao7274 <1980605903@qq.com>
2024-08-13 12:54:37 +0800

    项目初始化

---
+++ LxBox.js
@@ -0,0 +1,7 @@
+// LiteLoader-AIDS automatic generated
+/// <reference path="d:\BDS_api/dts/helperlib/src/index.d.ts"/> 
+ll.registerPlugin(
+/* name */ "LxBox", 
+/* introduction */ "一款基于LSE的空岛核心", 
+/* version */ [0, 0, 1], 
+/* otherInformation */ { "作者": "xiaoziyi", "版本": "测试版本" });
